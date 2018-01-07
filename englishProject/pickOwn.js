bce46fcd13fa589564924894a8e6ed9f0add634b
JonathanQuang <jquang@stuy.edu>
2018-01-07 00:06:46 -0500

    recursive button pressing works

--- englishProject/pickOwn.js
+++ englishProject/pickOwn.js
@@ -1,6 +1,4 @@
-
-
-var storyText = {"0":"You will be playing as Jane Re, following an alternate timeline where she never returned to Korea, but stays in New York City."}
+var score = 0;
 
 
 var insertText = function(theString){
@@ -18,7 +16,7 @@ var insertTextDel = function(theString){
 	for (i=0;i<10;i++){
 		try {
 			var deleteList = document.getElementById("del");
-			console.log(deleteList);
+			//console.log(deleteList);
 			deleteList.outerHTML = "";
 			delete deleteList;
 		}
@@ -27,16 +25,45 @@ var insertTextDel = function(theString){
 	}
 }
 
-var createButton = function(buttonText){
-	var btn = focument.createElement("BUTTON");
+var storyText = {
+	"0":"This is a test if your browser supports this program. If so, click on the button below. If not, switch to a different browser.",
+	"0options":"1.Proceed0",
+	"1":"You are Jane Re. You are together with Ed. Instead of being driven to 646 Thorn Street, Ed got off the bridge and went to the Promenade. The two of you look over the East River and the Manhattan skyline.",
+	"1options":"2.Let the silence carry on0,3.Talk about your dinner at Windows on the World1"
+	
+}
+
+var createButton = function(buttonText,functionString){
+	var btn = document.createElement("BUTTON");
 	var wrapper = document.createElement("div");
 	var textNode = document.createTextNode(buttonText);
 	btn.appendChild(textNode);
-	btn.onClick = "insertText('text junk')"
+	//btn.onclick = "function(){createButton('0');}"
+	btn.setAttribute('onclick',functionString);
 	btn.id = "del";
 	wrapper.append(btn);
 	var insertionPoint = document.getElementById("displayText");
-	insertionPoint.append(wrapper);
+	//insertTextDel(storyText[buttonText]);
+	insertionPoint.appendChild(wrapper);
 }
 
-createButton("0");
+createButton("start","parseJsonThing(0)");
+
+var parseJsonThing = function(eventIndex){
+	var displayText = storyText[eventIndex];
+	insertTextDel(displayText);
+	var arrayOfOptionsText = storyText[eventIndex+"options"];
+	var optionsArray = arrayOfOptionsText.split(',');
+	console.log(optionsArray);
+	for (recStr in optionsArray) {
+		var str = optionsArray[recStr];
+		console.log(str);
+		var firstPeriod = str.indexOf('.');
+		createButton(str.slice(firstPeriod+1,str.length-1),"parseJsonThing(" + str.slice(0,firstPeriod) + ")");
+
+	}
+
+}
+
+
+
