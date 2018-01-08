fc7f1f2b82e282c3928b2ed6ac536acff2e68931
JonathanQuang <jquang@stuy.edu>
2018-01-07 22:52:44 -0500

    draft

--- englishProject/pickOwn.js
+++ englishProject/pickOwn.js
@@ -31,8 +31,13 @@ var storyText = {
 	"1":"You are Jane Re. You are together with Ed. Instead of being driven to 646 Thorn Street, Ed got off the bridge and went to the Promenade. The two of you look over the East River and the Manhattan skyline.",
 	"1options":"2.Let the silence carry on0,3.Talk about your dinner at Windows on the World1",
 	"2":'Ed turns to you. "You look kinda down. Is something wrong?"',
+	"2options":"4.Tell him about your identity crisis2,5.Continue staying silent0",
 	"3":'You talk about how your grandfather did not like the steak. Ed comments on how nobody goes there for the steak and that the bar next door is better. His accent that he represses around Beth is noticeably present. The accent reminds you of Re Myungsun commenting on you looking more Korean.',
 	"3options":"2.Bite your lip at the memory of Re Myungsum's comment1",
+	"4":"You talk about your Korean mother and American father and all the friction it has caused in you life. Ed responds with an anecdote about his Italian mother and Irish father. He leans in closer to you, and tells you how much he missed you, blushing while doing so.",
+	"4options":"6.Tease him1",
+	"5":'Awkward silence begins to fill the space. You know it. He knows it too, but at least he tries to break the silence. Ed tells you how much he misses you, and blushes while doing so.'
+
 }
 
 var createButton = function(buttonText,functionString){
